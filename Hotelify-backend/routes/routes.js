6ea51a94925120873a4d1466bed6ee251b721281
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-03-19 11:54:07 -0500

    Routes Configuration | Gabriel Blanco

--- Hotelify-backend/routes/routes.js
+++ Hotelify-backend/routes/routes.js
@@ -9,3 +9,37 @@ app.get('/hotels', (request, response) => {
             response.send(result)
     })
 });
+
+app.post('/insert_hotel', (request, response) => {
+    console.log(request);
+    pool.query('INSERT INTO hotel(Name, Chain, Location, Rating) VALUES(?,?,?,?)',
+        request.body.name,
+        request.body.chain,
+        request.body.location,
+        request.body.rating,
+        (error, result) => {
+        if (error) throw error;
+            response.setHeader("Access-Control-Allow-Origin", "*")
+            response.status(201).send(`Hotel Inserted Successfully: ${result.insertId}`);
+        })
+    })
+
+app.post('/update_hotel', (request, response) => {
+    console.log(request);
+    pool.query(`UPDATE hotel SET Name = ?, Chain = ?, Locatoin = ?, Rating = ? WHERE Name = ${request.body.name}, Chain = ${request.body.chain}, Location = ${request.body.location}, Rating = ${request.body.rating}`,
+        (error, result) => {
+            if (error) throw error;
+            response.setHeader("Access-Control-Allow-Origin", "*")
+            response.status(201).send(`Hotel Updated: ${result.insertId}`);
+        })
+    })
+
+app.get('/delete_hotel', (request, response) => {
+    console.log(request);
+    pool.query(`DELETE FROM hotel WHERE Name = ${request.body.name}`,
+        (error, result) => {
+            if (error) throw error;
+            response.setHeader("Access-Control-Allow-Origin", "*")
+            response.status(201).send(`Hotel Deleted Successfully: ${result.insertId}`);
+        })
+    })
--- README.md
+++ README.md
@@ -7,4 +7,5 @@ AngularJS
 MySQL
 
 # Database Notes
-This database is running on local. So in the folder <pre>\sql-script\script.sql</pre> you will have access to the script for running on any MySQL server you want.
+This database is running on local. So in the folder <pre>\sql-script\script.sql</pre> you will have access to the script 
+for running on any MySQL server with default configurations you want.
