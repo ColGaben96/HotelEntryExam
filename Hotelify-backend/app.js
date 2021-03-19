72b788e3e570547940242a936522cb4a5fa4b8e8
Gabriel Blanco La Rotta <geblarot96@gmail.com>
2021-03-19 12:03:23 -0500

    app.js fix | Gabriel Blanco

--- Hotelify-backend/app.js
+++ Hotelify-backend/app.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Movify app listening at http://localhost:${port}`)
+    console.log(`Hotelify app listening at http://localhost:${port}`)
 })
